2a59db41b53a56bab9647ba081b362da66195352
bullishvince <vincent@bullishlife.com>
2024-09-22 00:34:48 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Elsa.Extensions;
+using Elsa.Workflows.Activities;
+using Elsa.Workflows.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+// Setup service container.
+var services = new ServiceCollection();
+
+// Add Elsa services to the container.
+services.AddElsa();
+
+// Build the service container.
+var serviceProvider = services.BuildServiceProvider();
+
+var workflow = new Sequence{
+    Activities = {
+        new WriteLine("Hello world!"),
+        new WriteLine("Goodbye world!")
+    }
+};
+
+// Resolve a workflow runner to execute the workflow.
+var workflowRunner = serviceProvider.GetRequiredService<IWorkflowRunner>();
+
+// Execute the workflow.
+await workflowRunner.RunAsync(workflow);
