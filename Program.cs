442ec7131a599dacef48e6de12f0e7e105980135
bullishvince <vincent@bullishlife.com>
2024-09-22 01:26:54 +0200

    setup elsa server

--- Program.cs
+++ Program.cs
@@ -1,20 +1,47 @@
 using Elsa.Extensions;
-using elsa_workflow_example.workflows;
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.EntityFrameworkCore.Modules.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddElsa(elsa => {
-    elsa.AddWorkflow<HelloWorld>();
+builder.Services.AddElsa(elsa =>
+{
+    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore());
+    elsa.UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore());
+    elsa.UseIdentity(identity =>
+    {
+        identity.TokenOptions = options => options.SigningKey = "sufficiently-large-secret-signing-key"; // This key needs to be at least 256 bits long.
+        identity.UseAdminUserProvider();
+    });
+    elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
+    elsa.UseWorkflowsApi();
+    elsa.UseRealTimeWorkflows();
+    elsa.UseCSharp();
     elsa.UseHttp();
+    elsa.UseScheduling();
+    elsa.AddActivitiesFrom<Program>();
+    elsa.AddWorkflowsFrom<Program>();
 });
 
+builder.Services.AddCors(cors => cors
+    .AddDefaultPolicy(policy => policy
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithExposedHeaders("x-elsa-workflow-instance-id"))
+);
+
+// Add Health Checks.
+builder.Services.AddHealthChecks();
+
+// Build the web application.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+// Configure web application's middleware pipeline.
+app.UseCors();
+app.UseRouting(); // Required for SignalR.
+app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
-app.UseWorkflows();
+app.UseWorkflowsApi(); // Use Elsa API endpoints.
+app.UseWorkflows(); // Use Elsa middleware to handle HTTP requests mapped to HTTP Endpoint activities.
+app.UseWorkflowsSignalRHubs(); // Optional SignalR integration. Elsa Studio uses SignalR to receive real-time updates from the server. 
 app.Run();
