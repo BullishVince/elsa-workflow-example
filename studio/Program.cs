2f888a4f55d5eca8a95a6cd36775b007bcca6375
bullishvince <vincent@bullishlife.com>
2024-09-22 02:27:28 +0200

    add studio as a Blazor Server

--- studio/Program.cs
+++ studio/Program.cs
@@ -1,28 +1,27 @@
+using Elsa.Studio.Core.BlazorServer.Extensions;
 using Elsa.Studio.Dashboard.Extensions;
-using Elsa.Studio.Shell;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
 using Elsa.Studio.Shell.Extensions;
 using Elsa.Studio.Workflows.Extensions;
-using Elsa.Studio.Contracts;
-using Elsa.Studio.Core.BlazorWasm.Extensions;
-using Elsa.Studio.Extensions;
-using Elsa.Studio.Login.BlazorWasm.Extensions;
-using Elsa.Studio.Login.HttpMessageHandlers;
 using Elsa.Studio.Workflows.Designer.Extensions;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 // Build the host.
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
-// Register root components.
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.RootComponents.RegisterCustomElsaStudioElements();
+// Register Razor services.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(options =>
+{
+    // Register the root components.
+    options.RootComponents.RegisterCustomElsaStudioElements();
+});
 
 // Register shell services and modules.
 builder.Services.AddCore();
-builder.Services.AddShell();
+builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
 builder.Services.AddRemoteBackend(
     elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler),
     options => configuration.GetSection("Backend").Bind(options));
@@ -30,12 +29,31 @@ builder.Services.AddLoginModule();
 builder.Services.AddDashboardModule();
 builder.Services.AddWorkflowsModule();
 
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize to handle large workflows.
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
 // Build the application.
 var app = builder.Build();
 
-// Run each startup task.
-var startupTaskRunner = app.Services.GetRequiredService<IStartupTaskRunner>();
-await startupTaskRunner.RunStartupTasksAsync();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 // Run the application.
-await app.RunAsync();
+app.Run();
